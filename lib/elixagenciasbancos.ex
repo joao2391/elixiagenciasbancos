8f9fff62abafd987061440573cd6a7110255b100
joao2391 <j_paulo_15@hotmail.com>
2022-02-20 07:26:40 -0300

    feat: add get agencias by municipio e bancos

--- lib/elixagenciasbancos.ex
+++ lib/elixagenciasbancos.ex
@@ -4,13 +4,7 @@ defmodule Elixagenciasbancos do
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Elixagenciasbancos.get_agencias_by_cep("03114-000", 10)
-      :world
-
+  Busca as agências localizadas de acordo com o CEP.
   """
   def get_agencias_by_cep(cep, quantidade_retorno \\ 1) when is_bitstring(cep) do
     case HTTPoison.get("https://olinda.bcb.gov.br/olinda/servico/Informes_Agencias/versao/v1/odata/Agencias?$top=#{quantidade_retorno}&$skip=0&$filter=Cep%20eq%20'#{cep}'&$format=json&$select=NomeIf,Segmento,NomeAgencia,Endereco,Numero,Complemento,Bairro,Cep,Municipio,UF,Telefone",
@@ -25,20 +19,17 @@ defmodule Elixagenciasbancos do
     end
   end
 
-   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Elixagenciasbancos.get_agencias_by_municipio("SAO PAULO", 10)
-      :world
+  @doc """
+  Busca as agências localizadas de acordo com o Municipio.
   """
   def get_agencias_by_municipio(municipio, quantidade_retorno \\ 1) when is_bitstring(municipio) do
-    case HTTPoison.get("https://olinda.bcb.gov.br/olinda/servico/Informes_Agencias/versao/v1/odata/Agencias?$top=#{quantidade_retorno}&$skip=0&$filter=Municipio%20eq%20'#{municipio}'&$format=json&$select=NomeIf,Segmento,NomeAgencia,Endereco,Numero,Complemento,Bairro,Cep,Municipio,UF,Telefone",
+
+    municipio_maiusculo = String.upcase(municipio)
+
+    case HTTPoison.get("https://olinda.bcb.gov.br/olinda/servico/Informes_Agencias/versao/v1/odata/Agencias?$top=#{quantidade_retorno}&$skip=0&$filter=Municipio%20eq%20'#{municipio_maiusculo}'&$format=json&$select=NomeIf,Segmento,NomeAgencia,Endereco,Numero,Complemento,Bairro,Cep,Municipio,UF,Telefone",
                         %{"User-Agent" => "elixagenciasbancos/1.0.1"}) do
       {:ok, %{body: raw_body, status_code: code}} -> {code, raw_body}
       retorno = Jason.decode!(raw_body)
-      IO.puts(code)
 
       retorno["value"]
 
@@ -47,6 +38,26 @@ defmodule Elixagenciasbancos do
     end
   end
 
-  #"https://olinda.bcb.gov.br/olinda/servico/Informes_Agencias/versao/v1/odata/Agencias?$top=#{quantidadeRetorno}&$skip=0&$filter=Municipio%20eq%20'#{municipio}'%20and%20contains(NomeIf%2C'#{nomeBanco}')'&$format=json&$select=NomeIf,Segmento,NomeAgencia,Endereco,Numero,Complemento,Bairro,Cep,Municipio,UF,Telefone"
+  @doc """
+  Busca as agências localizadas de acordo com o Municipio e Banco informados.
+  """
+  def get_agencias_by_municipio_e_nome_banco(municipio, nome_banco, quantidade_retorno \\ 1)
+                                              when is_bitstring(municipio) and is_bitstring(nome_banco) do
+
+    municipio_maiusculo = String.upcase(municipio)
+    nome_banco_maiusculo = String.upcase(nome_banco)
+
+    case HTTPoison.get("https://olinda.bcb.gov.br/olinda/servico/Informes_Agencias/versao/v1/odata/Agencias?$top=#{quantidade_retorno}&$skip=0&$filter=Municipio%20eq%20'#{municipio_maiusculo}'%20and%20contains(NomeIf%2C'#{nome_banco_maiusculo}')&$format=json&$select=NomeIf,Segmento,NomeAgencia,Endereco,Numero,Complemento,Bairro,Cep,Municipio,UF,Telefone",
+                        %{"User-Agent" => "elixagenciasbancos/1.0.1"}) do
+      {:ok, %{body: raw_body, status_code: code}} -> {code, raw_body}
+
+      retorno = Jason.decode!(raw_body)
+
+      retorno["value"]
+
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        IO.inspect reason
+    end
+  end
 
 end
