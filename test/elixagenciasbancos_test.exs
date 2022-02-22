09a42055cf056de6d22344844406af30708a776c
joao2391 <j_paulo_15@hotmail.com>
2022-02-22 08:19:21 -0300

    feat: add tests

--- test/elixagenciasbancos_test.exs
+++ test/elixagenciasbancos_test.exs
@@ -2,7 +2,28 @@ defmodule ElixagenciasbancosTest do
   use ExUnit.Case
   doctest Elixagenciasbancos
 
-  test "greets the world" do
-    assert Elixagenciasbancos.hello() == :world
+  test "retorna 03 agencias procuradas pelo CEP" do
+    assert length(Elixagenciasbancos.get_agencias_by_cep("04543-011", 3)) === 3
   end
+
+  test "retorna 01 agencia procurada pelo CEP - parametro default" do
+    assert length(Elixagenciasbancos.get_agencias_by_cep("04543-011")) === 1
+  end
+
+  test "retorna 01 agencia procurada pelo municipio" do
+    assert length(Elixagenciasbancos.get_agencias_by_municipio("aparecida", 1)) === 1
+  end
+
+  test "retorna 01 agencia procurada pelo municipio - parametro default" do
+    assert length(Elixagenciasbancos.get_agencias_by_municipio("aparecida")) === 1
+  end
+
+  test "retorna 01 agencia procurada pelo municipio e pelo nome do banco" do
+    assert length(Elixagenciasbancos.get_agencias_by_municipio_e_nome_banco("aparecida", "santander", 1)) === 1
+  end
+
+  test "retorna 01 agencia procurada pelo municipio e pelo nome do banco - parametro default" do
+    assert length(Elixagenciasbancos.get_agencias_by_municipio_e_nome_banco("aparecida", "santander")) === 1
+  end
+
 end
